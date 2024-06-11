07ee8d3868c71bc8f03a40cd3db8a3406f25af6c
Diluk-priyanathh <dilukpriyanath26@gmail.com>
2024-06-11 22:44:31 +0530

    Splash activity done

--- app/src/main/java/com/example/to_do_app_v01/MainActivity.java
+++ app/src/main/java/com/example/to_do_app_v01/MainActivity.java
@@ -1,24 +1,41 @@
 package com.example.to_do_app_v01;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final long SPLASH_DURATION = 3000; // 3 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Delay for 3 seconds and then start the RegisterActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", Context.MODE_PRIVATE); // Use "getSharedPreferences" directly
+                boolean LoggedIn = sharedPreferences.getBoolean("LoggedIn", false); // Default value should be false if key not found
+
+                // Start the LoggedIn activity based on login status
+                Intent intent;
+                if (LoggedIn) {
+                    // User is logged in, navigate to DashboardActivity
+                    intent = new Intent(MainActivity.this, DashboardActivity.class);
+                } else {
+                    // User is not logged in, navigate to RegisterActivity
+                    intent = new Intent(MainActivity.this, RegisterActivity.class);
+                }
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DURATION);
     }
 }
