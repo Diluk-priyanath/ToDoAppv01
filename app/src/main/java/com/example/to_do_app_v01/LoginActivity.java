29562297444e4afeb61014163920b01ae3146e0e
Diluk-priyanathh <dilukpriyanath26@gmail.com>
2024-06-13 00:28:30 +0530

    Add LoginActivity class structure

---
+++ app/src/main/java/com/example/to_do_app_v01/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.to_do_app_v01;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
