5beb61bb813f329ffb83d62fa257d44d2d0d6168
Diluk-priyanathh <dilukpriyanath26@gmail.com>
2024-06-12 19:55:45 +0530

    Add RegisterActivity class structure

---
+++ app/src/main/java/com/example/to_do_app_v01/RegisterActivity.java
@@ -0,0 +1,23 @@
+package com.example.to_do_app_v01;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
