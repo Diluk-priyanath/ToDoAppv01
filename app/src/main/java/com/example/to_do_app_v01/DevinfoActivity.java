950781aa90635d1322319bebcc497430a39cde1e
Diluk-priyanathh <dilukpriyanath26@gmail.com>
2024-06-13 21:22:47 +0530

    dev info activity

---
+++ app/src/main/java/com/example/to_do_app_v01/DevinfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.to_do_app_v01;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DevinfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_devinfo);
+    }
+}
