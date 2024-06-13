c60d5b69487d4aa7fea2cb107a7cba37e39acec1
Diluk-priyanathh <dilukpriyanath26@gmail.com>
2024-06-13 16:26:44 +0530

    Add DashboardActivity class definition and imports

---
+++ app/src/main/java/com/example/to_do_app_v01/DashboardActivity.java
@@ -0,0 +1,22 @@
+package com.example.to_do_app_v01;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.to_do_app_v01.databinding.ActivityDashboardBinding;
+
+import java.util.ArrayList;
+
+public class DashboardActivity extends AppCompatActivity {
+
+}
