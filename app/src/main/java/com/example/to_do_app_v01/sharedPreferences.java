d38200b53d857d362de058ee149f4038a6a0e2da
Diluk-priyanathh <dilukpriyanath26@gmail.com>
2024-06-13 15:53:50 +0530

    Add sharedPreferences class definition and imports

---
+++ app/src/main/java/com/example/to_do_app_v01/sharedPreferences.java
@@ -0,0 +1,12 @@
+package com.example.to_do_app_v01;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class sharedPreferences {
+
+}
