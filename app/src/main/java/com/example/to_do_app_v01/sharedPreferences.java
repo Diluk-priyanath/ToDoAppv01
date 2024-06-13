27efbef0a228d67d9a4704432dcf24c0c5f2bc4b
Diluk-priyanathh <dilukpriyanath26@gmail.com>
2024-06-13 15:54:35 +0530

    Add constant declarations and member variables

--- app/src/main/java/com/example/to_do_app_v01/sharedPreferences.java
+++ app/src/main/java/com/example/to_do_app_v01/sharedPreferences.java
@@ -8,5 +8,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class sharedPreferences {
+    private static final String PREF_NAME = "TODO_APP";
+    private static final String KEY_TODO_LIST = "TODO_LIST";
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
 
 }
