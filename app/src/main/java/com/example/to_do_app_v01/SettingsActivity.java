3d31dcf483a10c3a9bd457b488cd6f8aa5c84f2a
Diluk-priyanathh <dilukpriyanath26@gmail.com>
2024-06-13 21:14:31 +0530

    Add SettingsActivity class definition and imports

---
+++ app/src/main/java/com/example/to_do_app_v01/SettingsActivity.java
@@ -0,0 +1,20 @@
+package com.example.to_do_app_v01;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.to_do_app_v01.databinding.ActivitySettingsBinding;
+
+public class SettingsActivity extends AppCompatActivity {
+
+}
